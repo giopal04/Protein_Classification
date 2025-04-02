I really don't know what I did, but after removing the nn.Softmax module at the end of the network (because DeepSeek-R1 told me so) for the n-th time and after having fixed the accuracy, now logits look like they should (one large but < 10 positive, one large but > -10 negative) and accuracy finally reached 0.98 on the Symmetria-100k-easy astroid-citrus subset. Time to see if this works also for the proteins...

symmetria/decoders/prediction_head.py
@@ -3,11 +3,12 @@ from torch import nn
 
 
 class PredictionHead(nn.Module):
-    def __init__(self, input_size, output_size, use_bn=False):
+    def __init__(self, input_size, output_size, use_bn=False, use_relu=False):
         super().__init__()
         self.use_bn = use_bn
         self.input_size = input_size
         self.output_size = output_size
+        self.use_relu=use_relu
         if use_bn:
             self.decoder_head = torch.nn.Sequential(
                 nn.Linear(input_size, 512),
@@ -19,11 +20,15 @@ class PredictionHead(nn.Module):
                 nn.Linear(256, self.output_size),
             )
         else:
+            if use_relu:
+               actual_relu = nn.ReLU()
+            else:
+               actual_relu = nn.LeakyReLU()
             self.decoder_head = torch.nn.Sequential(
                 nn.Linear(input_size, 512),
-                nn.LeakyReLU(),
+                actual_relu,
                 nn.Linear(512, 256),
-                nn.LeakyReLU(),
+                actual_relu,
                 nn.Linear(256, self.output_size),
             )
 
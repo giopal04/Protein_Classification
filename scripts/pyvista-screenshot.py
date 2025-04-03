20bac7e68cc9dff5a07d0ebab3ea2bccf108f9aa
Avio <andreran@gmail.com>
2025-04-03 18:56:03 +0200

    Updated pyvista-screenshot.py to write output directly in <class> subdirectory...

--- scripts/pyvista-screenshot.py
+++ scripts/pyvista-screenshot.py
@@ -8,6 +8,10 @@ import argparse
 
 import time
 
+import cv2
+
+from pathlib import Path
+
 def start_plotting(filename, debug_show=False):
 	p = pv.Plotter(off_screen=(not debug_show))
 
@@ -41,9 +45,27 @@ def rotate(p, elevation, azimuth):
 	p.camera.elevation = elevation
 	p.camera.azimuth = azimuth
 
-def screenshot(p, out_fn):
+def screenshot(p, out_fn, ext='jpg', quality=90):
 	# Take a screenshot
-	p.screenshot(f'{out_fn}.png', transparent_background=True)
+	outf = f'{out_fn}.{ext}'
+	#print(f'Writing output to: {outf}')
+	img = p.screenshot(filename=None, transparent_background=True)
+	if ext == 'jpg':
+		#img = img.convert('RGB')
+		img = img[:,:,:3]
+		cv2.imwrite(outf, img, [int(cv2.IMWRITE_JPEG_QUALITY), quality])
+	else:
+		cv2.imwrite(outf, img)
+
+def get_class_and_id_from_filename(filename):
+	# 1768-60-6msg_32:TA:r_model1.vtk
+	# 2043-81-6qc2_24:X:B8_model1.vtk
+	# The first field is the id, the second one is the class
+	# Separator is '-'
+	fn = str(Path(filename).stem)
+	cls = fn.split('-')[1]
+	_id = fn.split('-')[0]
+	return cls, _id
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
@@ -57,13 +79,24 @@ if __name__ == '__main__':
 		'--debug_show', action='store_true', help='show plot',
 	)
 
+	# Add output directory --out_dir optional and non-positional parameter
+	parser.add_argument(
+		'--out_dir', type=str, help='output directory', default='/tmp/proteins-screenshots'
+	)
+
 	args = parser.parse_args()
 
 	filename = args.filename
+	cls, _id = get_class_and_id_from_filename(filename)
+	out_dir  = Path(args.out_dir) / cls
+	print(f'{_id} - {cls} - Creating directory: {out_dir}')
+	Path(out_dir).mkdir(parents=True, exist_ok=True)
 
 	for elevation in range(0, 360, 45):
 		for azimuth in range(0, 360, 45):
 			p = start_plotting(filename, args.debug_show)
-			print(f'{args.debug_show = }, {filename = }, {elevation = }, {azimuth = }')
+			#print(f'{args.debug_show = }, {filename = }, {elevation = }, {azimuth = }')
 			rotate(p, elevation, azimuth)
-			screenshot(p, f'{filename}-{elevation}-{azimuth}')
+			out_fn = Path(out_dir) / f'{Path(filename).stem}-{elevation}-{azimuth}'
+			#print(f'Output filename: {out_fn}')
+			screenshot(p, out_fn, ext='jpg', quality=90)
